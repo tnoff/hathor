964fdf3693d53fdc170670e5d2c4f45859400c3b
tnoff <tylernorth18@gmail.com>
2017-04-12 19:01:48 -0700

    update moviepy version

--- tests/utils.py
+++ tests/utils.py
@@ -24,13 +24,13 @@ def temp_audio_file(open_data=True, duration=2, suffix='.mp3', delete=True):
         audio_frames = lambda t: 2 *[np.sin(404 * 2 * np.pi * t)]
         audioclip = AudioClip(audio_frames, duration=duration)
         if suffix == '.mp3':
-            audioclip.write_audiofile(temp_file, verbose=False)
+            audioclip.write_audiofile(temp_file, progress_bar=False)
         else:
             image = ImageClip(np.random.rand(30, 30, 3) * 255)
             videoclip = image.set_audio(audioclip)
             videoclip.duration = duration
             videoclip.fps = 24
-            videoclip.write_videofile(temp_file, verbose=False)
+            videoclip.write_videofile(temp_file, progress_bar=False)
         try:
             if not open_data:
                 yield temp_file
