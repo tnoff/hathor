7415b88bd4f498ae6fffd95c536eac269be89d13
Tyler North <me@tyler-north.com>
2024-11-25 15:50:44 -0800

    wip audio done

--- tests/audio/test_audio.py
+++ tests/audio/test_audio.py
@@ -2,18 +2,14 @@ import os
 from tempfile import NamedTemporaryFile
 
 from mutagen.id3._util import ID3NoHeaderError
+import pytest
+
 
 from hathor.audio import metadata
 from hathor import utils
 from hathor.exc import AudioFileException
 
 from tests import utils as test_utils
-
-def mutagen_mock(_, **__):
-    return None
-
-def id3_mock(_, **__):
-    raise ID3NoHeaderError('')
 
 def test_audio_tags():
     with test_utils.temp_audio_file(suffix='.mp3') as temp_audio:
@@ -84,46 +80,9 @@ def test_audio_picture_extract_data_jpg():
                     new_temp_data = reader.read()
                 assert new_temp_data == temp_pic_data
 
-'''
-
-class TestAudio(test_utils.TestHelper):
-
-    def test_audio_picture_update_invalid_type(self):
-        # foo is a bad file to load from
-        with utils.temp_file(suffix='.foo') as test_file:
-            with test_utils.temp_audio_file(open_data=False) as temp_audio:
-                with self.assertRaises(AudioFileException) as error:
-                    metadata.picture_update(temp_audio, test_file)
-                self.check_error_message('Unsupported image type:%s' % test_file, error)
-
-        # test the ones that work
-        with test_utils.temp_image_file(suffix='.jpg') as test_file:
-            with test_utils.temp_audio_file(open_data=False) as temp_audio:
-                metadata.picture_update(temp_audio, test_file)
-        with test_utils.temp_image_file(suffix='.png') as test_file:
-            with test_utils.temp_audio_file(open_data=False) as temp_audio:
-                metadata.picture_update(temp_audio, test_file)
-
-    def test_audio_picture_extract_name_overriden(self):
-        with test_utils.temp_audio_file(open_data=False) as temp_audio:
-            # give it a bad ending, should be overriden with .jpg
-            with test_utils.temp_image_file() as temp_pic:
-                metadata.picture_update(temp_audio, temp_pic)
-                with utils.temp_file(suffix='.foo') as new_temp_pic:
-                    output = metadata.picture_extract(temp_audio, new_temp_pic)
-                    actual_path = output['output_path']
-                    self.assertNotEqual(actual_path, new_temp_pic)
-                    self.assertTrue(actual_path.endswith('.jpg'))
-                    # make sure file gets deleted
-                    os.remove('%s.jpg' % new_temp_pic)
-            # give it a bad ending, should be overriden with .png
-            with test_utils.temp_image_file(suffix='.png') as temp_pic:
-                metadata.picture_update(temp_audio, temp_pic)
-                with utils.temp_file(suffix='.foo') as new_temp_pic:
-                    output = metadata.picture_extract(temp_audio, new_temp_pic)
-                    actual_path = output['output_path']
-                    self.assertNotEqual(actual_path, new_temp_pic)
-                    self.assertTrue(actual_path.endswith('.png'))
-                    # make sure file gets deleted
-                    os.remove('%s.png' % new_temp_pic)
-'''
+def test_audio_picture_update_invalid_type():
+    with test_utils.temp_audio_file(suffix='.mp3') as temp_audio:
+        with NamedTemporaryFile(suffix='.foo') as temp_file:
+            with pytest.raises(AudioFileException) as error:
+                metadata.picture_update(temp_audio, temp_file.name)
+            assert 'Unsupported image type:' in str(error.value)
